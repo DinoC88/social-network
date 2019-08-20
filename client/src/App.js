0a97fee091e913abc036198d8d8affb5b8288668
Dino Coric <dino.coric1@hotmail.com>
2019-08-20 23:41:26 +0200

    Revert "Creating Navbar, Login and Register component"
    This reverts commit 78017a71db57f7bc888c21f554633607729bddd2.

--- client/src/App.js
+++ client/src/App.js
@@ -3,22 +3,13 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import './App.css';
 import Landing from './components/layout/Landing';
-import Footer from './components/layout/Footer';
-import Login from './components/auth/Login';
-import Register from './components/auth/Register';
-import Navbar from './components/layout/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Navbar />             
+      <Router>             
         <Route exact path="/" component={Landing} />
-        <div className="container">
-        <Route exact path="/register" component={Register} />
-        <Route exact path="/login" component={Login} />
-        </div>
-        <Footer />
+
       </Router>
 
     </div>
