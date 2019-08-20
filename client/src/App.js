ba86afe3609f49bf3aa6b8f6ffca5b8e34c53a35
Dino Coric <dino.coric1@hotmail.com>
2019-08-20 23:57:17 +0200

    Router and Navbar setup, Login and Register

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,23 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import './App.css';
 import Landing from './components/layout/Landing';
+import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
 
 function App() {
   return (
     <div className="App">
-      <Router>             
+      <Router>
+        <Navbar />
         <Route exact path="/" component={Landing} />
+          <div className="container">      
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
 
+          </div>
+        <Footer />
       </Router>
 
     </div>
